8177bb598c0a432b1b284ca1fd88958d6c8b2fe4
Anandhan <anand98bca@gmail.com>
2023-12-19 02:22:19 +0530

    Your revert ersion

--- backend/routs/auth.js
+++
@@ -1,6 +0,0 @@
-const express=require('express');
-const { registerUser } = require('../controllers/authController');
-const router=express.Router();
-
-router.route('/regester').post(registerUser)
-module.exports=router
